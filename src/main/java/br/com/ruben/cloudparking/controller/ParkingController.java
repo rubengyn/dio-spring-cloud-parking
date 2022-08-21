10f228ffede3b0d2b31fcdd4a53eb4e290ebe7cd
rubengyn <ruben.gyn@gmail.com>
2022-08-21 17:24:01 -0300

    Aula 06

---
+++ src/main/java/br/com/ruben/cloudparking/controller/ParkingController.java
@@ -0,0 +1,25 @@
+package br.com.ruben.cloudparking.controller;
+
+import br.com.ruben.cloudparking.model.Parking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    @GetMapping
+    public List<Parking> findAll(){
+        var parking = new Parking();
+        parking.setColor("Preto");
+        parking.setLicense("MSS-1111");
+        parking.setModel("VM GOL");
+        parking.setState("SP");
+
+        return Arrays.asList(parking,parking);
+    }
+}
